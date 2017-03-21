5577a1d7c40e100b3047a098c4bf88f9b0cc3234
Morgan Fogarty <mofo37@gmail.com>
2017-03-21 15:15:18 -0700

    connected Github API

--- starter-code/public/scripts/models/repo.js
+++ starter-code/public/scripts/models/repo.js
@@ -6,9 +6,16 @@
   repos.all = [];
 
   repos.requestRepos = function(callback) {
-    // TODO: How would you like to fetch your repos? Don't forget to call the callback.
-
+    $.ajax('https://api.github.com/users/mofo37/repos', {
+      method:'GET',
+      headers: {Authorization: 'token ' + token}
+    }).then((res) => {
+      console.log(res);
+    }) .catch((err) => {console.error})
   };
+repos.requestRepos();
+    // Done/TODO: How would you like to fetch your repos? Don't forget to call the callback.
+
 
   // REVIEW: Model method that filters the full collection for repos with a particular attribute.
   // You could use this to filter all repos that have a non-zero `forks_count`, `stargazers_count`, or `watchers_count`.
