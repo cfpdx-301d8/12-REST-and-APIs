1e379bb28a57c10c70a36fc1b4cd96ce7c764275
nicky evers <livelyevers@gmail.com>
2017-03-21 15:27:55 -0700

    got most functionality working, rendering is still not done

--- starter-code/public/scripts/models/repo.js
+++ starter-code/public/scripts/models/repo.js
@@ -6,9 +6,22 @@
   repos.all = [];
 
   repos.requestRepos = function(callback) {
+    $.ajax('https://api.github.com/users/neverssync/repos', {
+      method: 'GET',
+      headers: {
+        Authorization: `token ${token}`
+      }
+    })
+    .then((result) => {
+      repos.all.map(function() {
+        console.log(repos.all)
+        return result;
+      })
+    .catch(err => console.error)
     // TODO: How would you like to fetch your repos? Don't forget to call the callback.
-
-  };
+      callback();
+    });
+  }
 
   // REVIEW: Model method that filters the full collection for repos with a particular attribute.
   // You could use this to filter all repos that have a non-zero `forks_count`, `stargazers_count`, or `watchers_count`.
